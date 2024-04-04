b08e6c3694d2902ea57ad08a22612fc406b901ea
kuwa <02kuwa14@gmail.com>
2024-04-04 14:28:16 +0900

    first commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="l-footer p-footer">
+    <ul class="p-footer__nav">
+      <li><a href="page.html">ショップ情報</a></li>
+      <li><a href="#">ヒストリー</a></li>
+    </ul>
+    <small>Copyright: RaiseTech</small>
+  </footer>
+
+  <script src="js/jquery-3.7.1.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
---
+++ single.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hamburger|single</title>
+  <link rel="icon" href="images/favicon.svg">
+  <link rel="stylesheet" href="css/ress.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@700&display=swap" rel="stylesheet">
+  <script>
+    (function(d) {
+      var config = {
+        kitId: 'kky5huy',
+        scriptTimeout: 3000,
+        async: true
+      },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  </script>
+</head>
+
+<body>
+  <div class="l-container">
+    <div class="l-body">
+      <header class="l-header p-header">
+        <a href="index.html" class="p-header__title">Hamburger</a>
+        <form class="p-header__search">
+          <div class="p-searchBox">
+            <input type="search" class="c-input p-searchBox__input"><button type="submit" class="c-btn p-searchBox__btn">検索</button>
+          </div>
+        </form>
+        <button class="p-header__menuBtn js-menuBtn">Menu</button>
+      </header>
+    
+      <main class="l-main p-single">
+        <div class="c-mv p-single__mv">
+          <h1 class="p-single__title">h1 チーズバーガー</h1>
+        </div>
+
+        <div class="c-wrapper p-article__wrapper">
+          <article class="p-article">
+            <h2>見出しh2</h2>
+            <p class="c-margin--top">Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
+            <h3 class="c-margin--top">見出しh3</h3>
+            <h4 class="c-margin--top">見出しh4</h4>
+            <h5 class="c-margin--top">見出しh5</h5>
+            <h6 class="c-margin--top">見出しh6</h6>
+
+            <figure class="c-margin--top p-article__cite">
+              <blockquote>
+                <p>Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
+              </blockquote>
+              <figcaption class="c-margin--top">出典元：<cite><a href="#" class="cite__link">◯◯◯◯◯◯◯◯◯◯</a></cite></figcaption>
+            </figure>
+
+            <img src="images/single_articleImg.jpg" alt="ハンバーガーセット" class="c-margin--top p-article__img">
+
+            <figure class="c-margin--top p-figure -left">
+              <div class="p-figure__img">
+                <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              </div>
+              <figcaption class="p-figure__caption">テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</figcaption>
+            </figure>
+            
+            <figure class="c-margin--top p-figure -right">
+              <div class="p-figure__img">
+                <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              </div>
+              <figcaption class="p-figure__caption">テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります テキストが入ります</figcaption>
+            </figure>
+
+            <img src="images/single_article.jpg" alt="ハンバーガーセット" class="c-margin--top p-article__img--center">
+
+            <div class="c-margin--top p-article__gallery">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+              <img src="images/single_article.jpg" alt="ハンバーガーセット">
+            </div>
+
+            <ol class="c-margin--top p-article__list -ordered">
+              <li>リストリストリスト</li>
+              <li>
+                リストリストリスト
+                <ol>
+                  <li>リスト2リスト2リスト2</li>
+                  <li>リスト2リスト2リスト2</li>
+                </ol>
+              </li>
+              <li>リストリストリスト</li>
+              <li>リストリストリスト</li>
+            </ol>
+           
+            <ul class="c-margin--top p-article__list -unOrderd">
+              <li>リストリストリスト</li>
+              <li>
+                リストリストリスト
+                <ul>
+                  <li>リスト2リスト2リスト2</li>
+                  <li>リスト2リスト2リスト2</li>
+                </ul>
+              </li>
+              <li>リストリストリスト</li>
+              <li>リストリストリスト</li>
+            </ul>
+
+            <pre class="c-margin--top p-article__pre">
+<code>&lt;html&gt;
+  &lt;head&gt;
+  &lt;/head&gt;
+  &lt;body&gt;
+  &lt;/body&gt;
+&lt;/html&gt;</code></pre>
+
+          <table class="c-margin--top p-article__table">
+            <tbody>
+              <tr>
+                <td width="30%">テーブル</td>
+                <td>テーブル</td>
+              </tr>
+              <tr>
+                <td>テーブル</td>
+                <td>テーブル</td>
+              </tr>
+              <tr>
+                <td>テーブル</td>
+                <td>テーブル</td>
+              </tr>
+              <tr>
+                <td>テーブル</td>
+                <td>テーブル</td>
+              </tr>
+              <tr>
+                <td>テーブル</td>
+                <td>テーブル</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <button class="c-margin--top c-btn p-article__btn">ボタン</button>
+
+          <p class="c-margin--top p-article__bold">boldboldboldboldboldboldbold</p>
+
+          </article>
+        </div>
+      </main>
+    </div>
+
+    <aside class="p-nav l-sideBar">
+      <div class="p-nav__mask js-navMask"></div>
+      <nav class="p-menu l-menu js-menu">
+        <button class="p-menu__btn js-closeBtn">
+          <span>メニューを閉じる</span>
+        </button>
+        <h2>Menu</h2>
+        <ul class="p-menuList">
+          <li>
+            <h3><a href="archive.html">バーガー</a></h3>
+            <ul class="p-menuList__item">
+              <li><a href="#">ハンバーガー</a></li>
+              <li><a href="single.html">チーズバーガー</a></li>
+              <li><a href="#">テリヤキバーガー</a></li>
+              <li><a href="#">アボカドバーガー</a></li>
+              <li><a href="#">フィッシュバーガー</a></li>
+              <li><a href="#">ベーコンバーガー</a></li>
+              <li><a href="#">チキンバーガー</a></li>
+            </ul>
+          </li>
+          <li>
+            <h3><a href="#">サイド</a></h3>
+            <ul class="p-menuList__item">
+              <li><a href="#">ポテト</a></li>
+              <li><a href="#">サラダ</a></li>
+              <li><a href="#">ナゲット</a></li>
+              <li><a href="#">コーン</a></li>
+            </ul>
+          </li>
+          <li>
+            <h3><a href="#">ドリンク</a></h3>
+            <ul class="p-menuList__item">
+              <li><a href="#">コーラ</a></li>
+              <li><a href="#">ファンタ</a></li>
+              <li><a href="#">オレンジ</a></li>
+              <li><a href="#">アップル</a></li>
+              <li><a href="#">紅茶（Ice/Hot）</a></li>
+              <li><a href="#">コーヒー（Ice/Hot）</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </aside>
+  </div>
+
+  <footer class="l-footer p-footer">
+    <ul class="p-footer__nav">
+      <li><a href="page.html">ショップ情報</a></li>
+      <li><a href="#">ヒストリー</a></li>
+    </ul>
+    <small>Copyright: RaiseTech</small>
+  </footer>
+
+  <script src="js/jquery-3.7.1.min.js"></script>
+  <script src="js/main.js"></script>  
+</body>
+</html>
