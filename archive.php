281a00612e8e4b52265d9a65eab04d70f0e2df00
kuwa <02kuwa14@gmail.com>
2024-04-06 16:50:14 +0900

    調整

--- archive.php
+++ archive.php
@@ -19,31 +19,28 @@
                 while(have_posts()):
                   the_post();
             ?>           
-              <li <?php post_class('p-card'); ?>>
-                <div class="p-card__img">
-                  <?php the_post_thumbnail(); ?>
-                </div>
-                <div class="p-caption">
-                  <div class="p-caption__text">
-                    <h3><?php the_title(); ?></h3>
-                    <?php
-                      $get_post_subtitle = get_post_subtitle();
-                      if (!empty($get_post_subtitle)) {
-                        echo '<h4>' . $get_post_subtitle . '</h4>';
-                      }
-                    ?>
-                    <?php the_excerpt(); ?>                   
+                <li <?php post_class('p-card'); ?>>
+                  <div class="p-card__img">
+                    <?php the_post_thumbnail(); ?>
                   </div>
-                  <a href="<?php the_permalink(); ?>" class="c-btn--white p-caption__btn">詳しく見る</a>            
-                </div>             
-              </li>
-            <?php endwhile;
-              else :
-            ?>
-            <p><?php esc_html_e('No posts found.', 'textdomain'); ?></p>
-            <?php
-              endif;
-            ?>
+                  <div class="p-caption">
+                    <div class="p-caption__text">
+                      <h3><?php the_title(); ?></h3>
+                      <?php
+                        $get_post_subtitle = get_post_subtitle();
+                        if (!empty($get_post_subtitle)) {
+                          echo '<h4>' . $get_post_subtitle . '</h4>';
+                        }
+                      ?>
+                      <?php the_excerpt(); ?>                   
+                    </div>
+                    <a href="<?php the_permalink(); ?>" class="c-btn--white p-caption__btn">詳しく見る</a>            
+                  </div>             
+                </li>
+                <?php endwhile; 
+              else: ?>
+                <p>表示するメニューがありません</p>
+            <?php endif; ?>
             
             </ul>            
           </section>
