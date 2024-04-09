9c4a8ff06f1aec6728f1044107981b38926f1395
kuwa <02kuwa14@gmail.com>
2024-04-09 22:20:00 +0900

    トップページ用ファイル作成

---
+++ front-page.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+    
+      <main class="l-main p-main">
+        <div class="c-mv p-main__mv">
+          <h1>ダミーサイト</h1>
+        </div>
+
+        <div class="c-wrapper p-toc__wrapper">
+          <div class="p-toc">
+            <a href="<?php echo get_term_link( 6, 'post_tag'); ?>">
+              <section class="p-content -takeOut">
+                <h2>Take Out</h2>
+                <dl>
+                  <div class="p-content__dl">
+                    <dt>Take OUT</dt>
+                    <dd>当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています</dd>
+                  </div>
+                  <div class="p-content__dl">
+                    <dt>Take OUT</dt>
+                    <dd>当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで利用できる商品を掲載しています</dd>
+                  </div>
+                </dl>
+              </section>
+            </a>
+            <a href="<?php echo get_term_link( 7, 'post_tag'); ?>">
+              <section class="p-content -eatIn">
+                <h2>Eat In</h2>
+                <dl>
+                  <div class="p-content__dl">
+                    <dt>Eat In</dt>
+                    <dd>店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                  </div>
+                  <div class="p-content__dl">
+                    <dt>Eat In</dt>
+                    <dd>店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                  </div>
+                </dl>
+              </section>
+            </a>
+          </div>  
+        </div>    
+
+        <div class="p-about">
+          <div class="p-about__bg"></div>
+          <div class="c-wrapper p-about__wrapper">
+            <section class="p-about__text">
+              <h2>見出しが入ります</h2>
+              <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </section>
+          </div>
+        </div>
+      </main>
+    </div>
+    
+    <?php get_sidebar(); ?>
+
+    <?php get_footer(); ?>
